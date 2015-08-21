151fb650fb28937ecc91001af876e61a36f921ff
Justin Bakse <jbakse@Justins-iMac.local>
2015-08-21 17:04:25 -0400

    trying basedir template

--- gulpfile.js/config/index.js
+++ gulpfile.js/config/index.js
@@ -7,5 +7,7 @@ config.publicAssets    = config.publicDirectory
 config.sourceAssets    = config.sourceDirectory
 config.contentAssets    = config.contentDirectory
 
+config.base_url = "/"
+config.gh_pages_base_url = "/class_notes/"
 
 module.exports = config
--- gulpfile.js/tasks/markdown.js
+++ gulpfile.js/tasks/markdown.js
@@ -3,7 +3,8 @@ var browserSync  = require('browser-sync');
 var remarkable = require('gulp-remarkable');
 var wrap = require("gulp-wrap");
 var handleErrors = require('../lib/handleErrors');
-var config      = require('../config/markdown');
+var global_config = require('../config/');
+var config = require('../config/markdown');
 
 // var changed     = require('gulp-changed');
 
@@ -11,12 +12,11 @@ var config      = require('../config/markdown');
 
 
 gulp.task('markdown', function() {
-	console.log("markdown");
-
 	return gulp.src(config.src)
 		// .pipe(frontMatter({property: 'frontMatter', remove: true}))
 		.pipe(remarkable({preset: 'full', remarkableOptions: { html: true, linkify: true, typographer: false} }))
-		.pipe(wrap({src: config.template}))
+		.pipe(wrap({src: config.template}, {base_url: global_config.base_url}))
 		.pipe(gulp.dest(config.dest))
 		.pipe(browserSync.reload({ stream:true }));
 });
+//, {base_url: "base"}, {variable: "data"}

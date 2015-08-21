99067cda2e9115346107c666a6694298549121dd
Justin Bakse <jbakse@Justins-iMac.local>
2015-08-21 15:57:37 -0400

    Update 2015-08-21T19:57:36.572Z

--- gulpfile.js/config/copy_source.js
+++
@@ -1,6 +0,0 @@
-var config = require('./')
-
-module.exports = {
-  src: [config.sourceAssets + '/**/*', config.contentAssets + '/**/*.html', config.contentAssets + '/**/*.js'],
-  dest: config.publicAssets
-}
